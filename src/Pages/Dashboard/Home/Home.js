17f8707d5bc98e276b27f28c877e1313741a6aec
ishrafil2233 <ishrafil2233@gmail.com>
2022-04-22 01:39:17 +0600

    update hosting

--- src/Pages/Dashboard/Home/Home.js
+++ src/Pages/Dashboard/Home/Home.js
@@ -1,50 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 const Home = () => {
-    const [selectedDepartment, setSelectedDepartment] = useState(null);
-    const [users, setUsers] = useState([]);
-
-    useEffect(() => {
-        const url = `https://shrouded-spire-42050.herokuapp.com/users`;
-        fetch(url)
-            .then(res => res.json())
-            .then(data => setUsers(data))
-    }, [])
-
-    const userOptions = new Map([
-        ...users.map(user => [user.department, user.department])
-    ]);
-
-    const filteredFiles = () => {
-        return users.filter(user => String(user.department) === selectedDepartment);
-    };
-    // filteredFiles().map(file => console.log(file.displayName))
-
     return (
         <div>
-            <select
-                onChange={({ target }) => setSelectedDepartment(target.value)}
-            >
-                <option value=''>--Select Department--</option>
-                {[...userOptions].map(([department, displayName]) => (
-                    <option
-                        value={department}>
-                        {displayName}
-                    </option>
-                ))}
-            </select>
-
-            <br />
-
-            <select>
-                <option value=''>--Select Name--</option>
-                {filteredFiles().map(file => (
-                    <option value={file.displayName}>
-                        {file.displayName}
-                    </option>
-                ))}
-            </select>
-
+            This is Home
         </div>
     );
 };
